4c6044b8227be8c857e70fd63b3b637c5443718e
Noah-234 <noah.ouellette@stu.ocsb.ca>
2021-12-02 22:41:20 +0000

    Created and finished program to explain local and global variables in C++

---
+++ scopeOfVariables.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 Noah Ouellette All rights reserved.
+//
+// Created by: Noah Ouellette
+// Created on: Dec. 1 2021
+// This program shows how local and global variables work
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+
+void globalVariable() {
+    // this shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+
+    localVariable();
+    globalVariable();
+}
